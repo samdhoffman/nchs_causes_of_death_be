e554165ff0d290e27530955174358e91bfe8c5b4
Sam Hoffman <samdhoffman@gmail.com>
2020-03-23 16:33:47 -0400

    Remove comment

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-# Disclaimer: 
-# I don't always feel it is necessary to use comments as they can be misleading as code changes over time
-# For purposes of documenting the code structure and thought process I have included comments below
-
 # This file will be responsible for running our application
 
 from nchs_causes_of_death import app
